69bc3148ffd2ac756572c6ed2c4f3ab6e8fcdb26
Juergen Bohl <jbohl@h-quer.de>
2019-03-15 23:43:28 +0100

    intial commit

--- JsonNetTest/Program.cs
+++ JsonNetTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JsonNetTest
 {
@@ -10,6 +11,16 @@ namespace JsonNetTest
     {
         static void Main(string[] args)
         {
+            var d = new TestData();
+            d.Dictionary.Add(1,new TestDataItem(){Boolean = true});
+
+            string json = JsonConvert.SerializeObject(d);
+
+            var dDs = JsonConvert.DeserializeObject<TestData>(json);
+
+            var dDsA = JsonConvert.DeserializeObject(json);
+
+
         }
     }
 }
---
+++ JsonNetTest/TestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonNetTest
+{
+    public class TestData
+    {
+        public TestData()
+        {
+            this.Dictionary=new Dictionary<int, TestDataItem>();
+        }
+
+        public int? Test { get; set; }
+
+        public Dictionary<int,TestDataItem> Dictionary { get;  }
+    }
+
+    public class TestDataItem
+    {
+        public bool Boolean { get; set; }
+
+        public int? NullableInt { get; set; }
+
+        public string Text { get; set; }
+    }
+}
